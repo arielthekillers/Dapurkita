25e6e9f94da06903b1d6d80606b3724bbef8e3ee
arielthekillers <arielthekillers@gmail.com>
2025-01-04 23:23:03 +0800

    Add category modules

---
+++ database/migrations/2025_01_04_144606_create_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('category', length: 50);
+            $table->string('slug', length: 100);
+            $table->timestamps();
+            $table->softDeletes('deleted_at', precision: 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
